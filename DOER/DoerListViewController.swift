26dadb1620d95a461a8eecb47cd44968b6debccd
Alex Lee <alexytlee@gmail.com>
2019-02-03 11:11:19 -0800

    Initial commit of free to-do app with datasource and delegate methods

---
+++ DOER/DoerListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  DOER
+//
+//  Created by Alex Lee on 2019-02-03.
+//  Copyright © 2019 Alex Lee. All rights reserved.
+//
+
+import UIKit
+
+class DoerListViewController: UITableViewController {
+    
+    let itemArray = ["Find Alex", "Buy Pizza", "Rule the World"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DoerItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - Tableview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+}
+
